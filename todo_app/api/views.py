3d42c1f0da671765cd6cf7fbc3f26ce5a8f4432f
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-23 11:12:30 +0400

    order task by DESC and fix typo

--- todo_app/api/views.py
+++ todo_app/api/views.py
@@ -20,7 +20,7 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
@@ -50,6 +50,6 @@ def taskUpdate(request, pk):
 @api_view(['DELETE'])
 def taskDelete(request, pk):
     task = Task.objects.get(pk=pk)
-    serializer.delete()
+    task.delete()
 
     return Response('item deleted successfuly!')
