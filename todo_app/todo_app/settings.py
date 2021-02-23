ebbd093dbca02db91d1ebf2a3dc732e948fecda5
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-23 11:14:40 +0400

    add todo to installed apps

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework',
+    'todo_frontend.apps.TodoFrontendConfig',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
